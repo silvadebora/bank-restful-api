f70155f14d3ce82acdae57404eb89bd605759050
Debora <silvadeborafisio@gmail.com>
2024-06-11 16:29:09 -0300

    fix: fix values of precision and scale

--- src/main/java/com/dio/bank/domain/model/Card.java
+++ src/main/java/com/dio/bank/domain/model/Card.java
@@ -11,28 +11,24 @@ public class Card {
     private Long id;
     @Column(unique = true)
     private String number;
-    @Column(name = "available_limit", scale = 13, precision = 2)
+    @Column(name = "available_limit", precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
     public String getNumber() {
         return number;
     }
-
     public void setNumber(String number) {
         this.number = number;
     }
-
     public BigDecimal getLimit() {
         return limit;
     }
-
     public void setLimit(BigDecimal limit) {
         this.limit = limit;
     }
